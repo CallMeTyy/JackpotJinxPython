defc25900a952b5173455299e51b629b68f3b67d
Ty <500805@student.saxion.nl>
2023-06-20 11:00:56 +0200

    Initial Decoding + Data

--- endecoder.py
+++ endecoder.py
@@ -1,3 +1,6 @@
+import constants
+import re
+
 
 
 def Decode(inputData):
@@ -12,8 +15,39 @@ def Encode(processedData):
     # Encode the processed data into a simple protocol to send back to the arduino
     return encodedData
 
+
+
 def decode_header(input):
     """ checks what the header of the message is. After that, goes to specific decoder for the rest"""
-    head = input[:3]
-    tail = input[3:]
+    head = input[:constants.HEADER_LENGTH]   
+    tail = input[constants.TAIL_LENGTH:]
+    match head:
+        case "LE":
+            decode_lever(input, tail)            
+        case "BT":
+            decode_button(input, tail)
+        case "RL":
+            decode_reel(input, tail)
+        case "SY":
+            decode_sys(input, tail)      
+        
+
+def decode_lever(input, tail):
+    """Specific decoder for the lever"""
+    print(f"LEV {tail}")
+
+def decode_button(input, tail):
+    """Specific decoder for the button"""
+    headerdata = input[:constants.HEADER_LENGTH+1][2:]
+    print(f"BT{headerdata} {tail}")
+
+def decode_reel(input, tail):
+    """Specific decoder for the reel"""
+    headerdata = input[:constants.HEADER_LENGTH+1][2:]
+    print(f"RL{headerdata} {tail}")
+
+def decode_sys(input, tail):
+    """Specific decoder for the system"""
+    print(f"Sys {tail}")
+    
 
