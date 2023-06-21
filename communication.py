78312c53d26abf6c30b153fea29b6107834cdb99
hatos <s1978497@student.utwente.nl>
2023-06-21 10:36:15 +0200

    data com works now

--- communication.py
+++ communication.py
@@ -18,10 +18,11 @@ def get_port():
 
 
 def read_incoming():
-    msg = arduino.readline()[:-2]  # the last bit gets rid of the new-line chars
+    msg = arduino.readline()[:]  # the last bit gets rid of the new-line chars
     data_str = str(msg)[2:-1]
     if len(data_str) > 0:
-        data_str = str(msg)[2:-1]
+        # data_str = str(msg)[2:-1]
+        data_str = data_str[:-4]
         # if constants.DEBUG:
         #     print(data_str)
         return data_str
@@ -36,32 +37,22 @@ def process_data():
     msg = ""
     while True:
         msg_in = read_incoming()
-        msg += str(msg_in)
         if len(msg_in) > 0:
-            word_end_pos = msg.find("\n")
-            command = msg[:word_end_pos+1]
-            msg = msg[word_end_pos+1:]
-            print(command)
-            print(msg)
-            endecoder.decode(command)
-        process_loop()
+            print(msg_in)
+            # word_end_pos = msg.find("\r\n")
+            # command = msg[:word_end_pos+2]
+            # msg = msg[word_end_pos+2:]
+            # print(command)
+            # print(msg)
+            endecoder.decode(msg_in)
 
 
-def process_loop():
-    pass
-
-
-# initialisiation
-if constants.SIMULATED_SERIAL:
-    print("Serial now simulated")
-else:
+def initialise(baudrate):
     port = get_port()
     baud_rate = constants.BAUD_RATE
     arduino = serial.Serial(port=port, baudrate=baud_rate, timeout=0)
+    return arduino
 
-send_outgoing("hello!")
-read_incoming()
-process_data()
 
 
 
