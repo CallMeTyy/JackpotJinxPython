e79a6a02d44fbd911d062f13356d8779d98e15c0
Ty <500805@student.saxion.nl>
2023-06-23 12:41:37 +0200

    Comments for communication, constants, endecoding and main

--- communication.py
+++ communication.py
@@ -10,6 +10,7 @@ import time
 class Communication:
 
     def get_port(self):
+        """Retrieves all ports from device and returns the port applicable for the device OS"""
         ports = list(serial.tools.list_ports.comports())
         for p in ports:
             short = str(p).split(' ')[0]
@@ -20,13 +21,15 @@ class Communication:
                 return short
             elif short == constants.ARDUINO_PORT_WINDOWS:
                 return short
+            elif short == constants.ARDUINO_PORT_RASPI:
+                return short
 
 
     def read_incoming(self, arduino):
+        """Reads all incoming messages and decodes them into strings"""
         msg = arduino.readline()[:]  # the last bit gets rid of the new-line chars
         data_str = str(msg)[2:-1]
         if len(data_str) > 0:
-            # data_str = str(msg)[2:-1]
             data_str = data_str[:-4]
             if constants.COMM_DEBUG:
                 print("in:" + data_str)
@@ -34,11 +37,13 @@ class Communication:
         return data_str
 
     def send_outgoing(self, msg, arduino):
+        """Sends the message to the specified arduino"""
         if constants.COMM_DEBUG:
             print("out:" + msg)
         arduino.write(str.encode(msg + '\n'))
 
     def initialise(self, baudrate):
+        """Retrieves the port and sets up the arduino for communication"""
         port = self.get_port()
         baud_rate = constants.BAUD_RATE
         arduino = serial.Serial(port=port, baudrate=baud_rate, timeout=1)
