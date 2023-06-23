8e22ee07c6730a05261008c500235d6a44d463b4
Teun de Vries <devries.teun23@gmail.com>
2023-06-23 11:48:23 +0200

    Added comments to dataset script, as well as additional check if year is in range

--- Dataset.py
+++ Dataset.py
@@ -4,29 +4,36 @@ import re
 
 import constants
 
+# Dataset for Britain. If data is missing a value of 0 is used.
 dataBritain = DataFrame([[75.68,37.01,202.70,177.16],[89.94,38.74,193.69,187.45],[77.00,32.61,168.34,156.74],[73.87,33.08,177.64,160.07],[68.96,27.44,190.99,114.99],[21.64,14.21,240.57,59.76],[67.45,27.02,288.47,147.93]], columns=(0,1,2,3))
-# print(dataBritain)
+# Dataset for Nevada. If data is missing a value of 0 is used.
 dataNevada = DataFrame([[296.72,0.93, 0.0,7.54],[349.62,1.05,0.0,8.25],[350.68,1.23,0.0,8.89],[317.16,1.16,0.0,9.17],[303.62,1.17,0.0,9.30],[468.07,1.64,0.0,24.18],[448.20,2.04,0.0,22.53]], columns=(0,1,2,3))
-# print(dataNevada)
+# Dataset for Macau. If data is missing a value of 0 is used.
 dataMacau = DataFrame([[765.75, 0.0,0.02,3.20],[817.81,0.0,0.03,3.34],[958.74,0.0,0.04,3.08],[863.39,0.0,0.04,2.91],[792.42,0.0,0.04,2.28],[1088.05,0.0,0.23,10.69],[1484.64,0.0,0.27,11.55]], columns=(0,1,2,3))
-# print(dataMacau)
+# Dataset for Australia. If data is missing a value of 0 is used.
 dataAustralia = DataFrame([[627.86,13.73,77.74,146.14],[600.11,13.87,71.02,155.18],[627.52,13.87,74.62,176.16],[582.79,12.94,85.67,166.78],[423.80,10.38,81.10,141.15],[0.0, 0.0, 0.0, 0.0],[0.0, 0.0, 0.0, 0.0]], columns=(0,1,2,3))
-# print(dataAustralia)
+# Create tuple holding all of the datasets.
 data = (dataBritain,dataNevada,dataMacau,dataAustralia)
 
-
+# Function to retrieve the data from the dataset. Returns float.
 def fetch_data(input : tuple):
     if constants.AUDIO_DEBUG:
         print("final reel values = " + str(input))
+    # Check if input are 3 ints and not something else.
     if(re.fullmatch("\([0-9], [0-9], [0-9]\)", str(input))):
+        #Check if datapoints are out of range.
         if(input[0] >= len(data)):
             print("Country not in dataset...")
         elif(input[1] >= len(data)):
             print("Game not found in dataset...")
+        elif(input[2] > 7):
+            print("Year not found in dataset...")
         else:
+            # if datapoints are in range, select the datapoint based on the input values.
             country = data[input[0]]
             game = country[input[1]]
             money_lost = game[input[2]]
+            #return the money lost in the selected datapoint.
             return money_lost
     else:
         print("Input not accepted, it should be in the form of int,int,int")
